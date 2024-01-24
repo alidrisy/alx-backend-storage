cd1ae67b5e1db5ad5051d4afa13aca702c27d46b
alidrisy <a.alidrisy6@gmail.com>
2024-01-24 23:13:30 +0300

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,17 +2,29 @@
 """ Model for the get_page function """
 import redis
 from requests import get
+from functools import wraps
+from typing import Callable
 
 
+def cache_content(fn: Callable) -> Callable:
+    """fuction to cache the content of respons"""
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """wraper to check if the content exist or not"""
+        r = redis.Redis()
+        content = r.get(url)
+        name = f"count:{url}"
+        r.incr(name)
+        if content:
+            return content.decode('utf-8')
+        content = fn(url)
+        r.setex(url, 10, content)
+        r.set(f'count:{url}', 0)
+        return content
+    return wrapper
+
+
+@cache_content
 def get_page(url: str) -> str:
     """obtain the HTML content of a particular URL and returns it."""
-    r = redis.Redis()
-    content = r.get(url)
-    name = f"count:{url}"
-    r.incr(name)
-    if content:
-        return content.decode('utf-8')
-    content = get(url)
-    r.setex(url, 10, content.text)
-    r.set(f'count:{url}', 0)
-    return content.text
+    return get(url).text
